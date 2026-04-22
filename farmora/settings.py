c94ace91e4306f39e8bc64da85f37988bff92adf
1tharun <tharun2004d@gmail.com>
2026-04-22 10:13:44 +0530

    update nav farmOra

--- farmora/settings.py
+++ farmora/settings.py
@@ -83,7 +83,7 @@ STATICFILES_DIRS = [STATIC_DIR] if os.path.exists(STATIC_DIR) else []
 STATIC_URL = '/static/'
 
 STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
